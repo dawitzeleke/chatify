3246c6f5e1a970a041120bb1d5356890ffd9ea46
dz <dawitzeleke92@gmail.com>
2025-09-30 15:47:47 +0300

    login and logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,6 +7,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
 
     checkAuth: async () => {
         try {
@@ -20,6 +21,18 @@ export const useAuthStore = create((set) => ({
         }
     },
 
+    login : async(data) => {
+        set({isLoggingIn: true});
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({authUser: res.data});
+            toast.success("Logged in successfully!");
+        } catch (error) {
+            toast.error(error?.response?.data?.message || "Something went wrong");
+        } finally {
+            set({isLoggingIn: false});
+        }
+    },
     signup : async(data) => {
         set({isSigningUp: true});
         try {
@@ -31,5 +44,15 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({isSigningUp: false});
         }
-    }
+    },
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully!");
+        } catch (error) {
+            toast.error(error?.response?.data?.message || "Something went wrong");
+        }
+    },
+
 }));
