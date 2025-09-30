803cc452712a0f40cf7f324af6ef58d6224b8219
dawitzeleke <dawitzeleke92@gmail.com>
2025-09-30 16:05:31 +0300

    fix typo

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router";
 
 function LoginPage() {
   const [formData, setFormData] = useState({ email: "", password: "" });
-  const { login, isLogginIn } = useAuthStore();
+  const { login, isLoggingIn } = useAuthStore();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -63,7 +63,7 @@ function LoginPage() {
                   </div>
 
                   {/* SUBMIT BUTTON */}
-                  <button className="auth-btn" type="submit" disabled={isLogginIn}>
+                  <button className="auth-btn" type="submit" disabled={isLoggingIn}>
                     {isLogginIn ? (
                       <LoaderIcon className="w-full h-5 animate-spin text-center" />
                     ) : (
