c55584f946de9fbdf506900074527a7288d9397d
dawitzeleke <dawitzeleke92@gmail.com>
2025-10-06 10:52:10 +0300

    realtime message implemented

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,8 @@
 import Message from '../models/Message.js';
 import User from '../models/User.js';
 import cloudinary from 'cloudinary';
+import { io, getReceiverSocketId } from '../lib/socket.js';
+
 // ...existing code...
 
 export const getAllContacts = async (req, res) => {
@@ -64,6 +66,12 @@ export const sendMessage = async (req, res) => {
         });
 
         await newMessage.save();
+
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+
         res.status(201).json(newMessage);
     } catch (error) {
         res.status(500).json({ message: 'Internal server error' });
